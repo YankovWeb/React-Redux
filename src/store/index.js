b37c2ccbf5a530f88f075706ecd4f70d2f05d6e3
Grigor Yankov <yankovweb@outlook.com>
2023-01-20 19:18:32 +0200

    Store

--- src/store/index.js
+++ src/store/index.js
@@ -7,11 +7,16 @@ const counterReducer = (state = {counter: 0}, action) => {
     };
   }
 
-  if (action.type === "decre,emt") {
+  if (action.type === "decrement") {
     return {
       counter: state.counter - 1,
     };
   }
+  if (action.type === "increase") {
+    return {counter: state.counter + action.amount};
+  }
+
+  return state;
 };
 
 const store = createStore(counterReducer);
